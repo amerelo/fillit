05c098ff2dd7978e44e6db8ec2d068e6d7359f7a
Alexis MERELO <amerelo@e1r11p20.42.fr>
2015-12-11 07:17:42 +0100

    2 commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: amerelo <amerelo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/04 15:01:57 by amerelo           #+#    #+#             */
-/*   Updated: 2015/12/04 15:02:01 by amerelo          ###   ########.fr       */
+/*   Updated: 2015/12/10 05:49:36 by amerelo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,33 +52,88 @@ int		check_block(char *str)
 			count_diezes++;
 		count_chars++;
 	}
-	ft_putnbr(count_diezes);
 	if (count_chars == 20 && count_diezes == 5)
 		return (1);
 	return (0);
 }
 
 
-/* typedef struct		s_list
+
+
+t_tetri	*create_list_element(char  *tetriminos, int x)
 {
-	char            *tetri;
-	int				tetri_nuber;
-	struct s_list	*next;
-}					t_tetri;
+	t_tetri *new_tetri;
 
-t_list	*save_tetri(char tmp, int x)
-{
-	t_tetri *tetri;
-
-
+	new_tetri = (t_tetri *)malloc(sizeof(t_tetri));
+	if (!new_tetri)
+		return (NULL);
+	new_tetri->tetri = tetriminos;
+	new_tetri->tetri_nuber = x;
+	new_tetri->next = NULL;
+	return (new_tetri);
 }
-*/
 
-int		check_tetri(char *str)
+t_tetri *add_on_list(t_tetri *head, t_tetri *l_tmp, int x)
+{
+	t_tetri *tmp;
+
+	if (x != 0)
+	{
+		tmp = head;
+		while (head->next != NULL)
+			head = head->next;
+		head->next = (t_tetri *)malloc(sizeof(t_tetri));
+		head->next = l_tmp;
+		return (tmp);
+	}
+	else
+		head = l_tmp;
+	return (head);
+}
+
+//int test_plus(t_tetri *mainlist, int *final_carre)
+//{
+//}
+
+void test(t_tetri *mainlist, int carre)
 {
 	int x;
 	int i;
-	char *tmp;
+	char **final_carre;
+
+	/*final_carre = (char **)ft_memalloc(sizeof(char *) * ((carre + 1) * 4));
+	
+	i = 0;
+	x = 0;
+	while (mainlist->tetri[i])
+	{	
+		while (mainlist->tetri[x] != '#')
+			x++;
+		if (mainlist->tetri[i] == '#')
+		{
+
+		}
+		i++;
+	}*/
+
+	while (mainlist != NULL)
+	{
+		x++;
+		ft_putchar(mainlist->tetri_nuber + 'A');
+		ft_putchar('\n');
+		ft_putendl(mainlist->tetri);
+		ft_putchar('\n');
+		mainlist = mainlist->next;
+	}
+}
+
+int		check_tetri(char *str)
+{
+	int			x;
+	int			i;
+	char		*tmp;
+	t_tetri 	*head;
+	t_tetri		*l_tmp;
 
 	x = 0;
 	while(str[x])
@@ -86,24 +141,26 @@ int		check_tetri(char *str)
 		if (!(tmp = (char *)ft_memalloc(sizeof(char) * 21)))
 			return (0);
 		i = 0;
-		while (i < 20 && str[i + x] && (str[i + x] == '.' || str[i + x] == '#' || str[i + x] == '\n'))
+		while (i < 20 && str[i + x] && (str[i + x] == '.' || str[i + x] == '#' ||
+		 str[i + x] == '\n'))
 		{
 			tmp[i] = str[i + x];
 			i++;
 		}
 		x += i;
 		if(i != 20 || !(check_block(tmp)))
-		{
-			ft_putendl("ERROR");
 			return (0);
-		}
-	//	ft_putnbr(x % 20);
-		ft_putchar('\n');
-		ft_putendl(tmp); //  save_tetri(tmp, (x % 20))
+		//ft_putnbr(x % 20);
+		//ft_putchar('\n');
 		if (str[x] == '\n' && str[x + 1] != '.' && str[x + 1] != '#')
 			return (0);
+		l_tmp = create_list_element(tmp, (x % 20));
+		head = add_on_list(head	, l_tmp, (x % 20));
+
 		x++;
 	}
+	x--;
+	test (head, (x % 20 + 1) );
 	return (1);
 }
 
@@ -123,7 +180,7 @@ int		main(int ac, char **av)
 			buf[r] = '\0';
 			if (!check_tetri(buf))
 			{
-				ft_putendl("ERROR");
+				ft_putendl("error");
 				close(fi);
 				return (-1);
 			}
